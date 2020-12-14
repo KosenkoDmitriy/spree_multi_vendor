3712e54791a9be5602ae0a5d7eab9c702564a9e9
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-12-14 20:27:10 +0300

    test_app

---
+++ spec/dummy/config/initializers/devise.rb
@@ -0,0 +1,3 @@
+if Object.const_defined?("Devise")
+  Devise.secret_key = "093f59eef9a3a0699b320d55abce73d65adeace6c7beea60d9bd93ccd613bb40ceb88722b998b7dfc8534fa7463a6b3ed747"
+end
---
+++ spec/dummy/vendor/assets/javascripts/spree/frontend/country_state.js
@@ -0,0 +1,40 @@
+//= require select2
+
+function update_state(region, done) {
+  'use strict'
+  var region = ''
+  // var country = $('span#' + region + 'country .select2').select2('val')
+  var country = $('span#' + region + 'country .select2 option:selected').val()
+  var stateSelect = $('span#' + region + 'state select.select2')
+  var stateInput = $('span#' + region + 'state input.state_name')
+
+  $.get(Spree.routes.states_search + '?country_id=' + country, function (data) {
+    var states = data.states
+
+    if (states.length > 0) {
+      stateSelect.html('')
+      var statesWithBlank = [{
+        name: '',
+        id: ''
+      }].concat(states)
+      $.each(statesWithBlank, function (pos, state) {
+        var opt = $(document.createElement('option'))
+          .prop('value', state.id)
+          .html(state.name)
+        stateSelect.append(opt)
+      })
+      stateSelect.prop('disabled', false).show()
+      // stateSelect.select2()
+      stateInput.hide().prop('disabled', true)
+    } else {
+      stateInput.prop('disabled', false).show()
+      stateSelect.select2('destroy').hide()
+    }
+
+    if (done) done()
+  })
+}
+
+$(document).on('ready turbolinks:load', function () {
+  $('span#country .select2').on('change', function () { update_state(''); });
+})
