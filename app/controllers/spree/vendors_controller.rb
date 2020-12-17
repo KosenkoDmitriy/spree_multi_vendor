9bfcfe14764d7ded142b371b7c3229302317ca26
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-12-17 15:29:45 +0300

    test to remember image and checkbox state after validation error

--- app/controllers/spree/vendors_controller.rb
+++ app/controllers/spree/vendors_controller.rb
@@ -4,12 +4,90 @@ class Spree::VendorsController < Spree::StoreController
     # @countries_array = Spree::Country.all.map { |country| [country.name, country.id] }
     # @states_array = Spree::State.all.map { |state| [state.name, state.id] }
     @vendor = Spree::Vendor.new
+    # @vendor.build(:stock_locations)
+  end
+
+  def create2
+    # @user = Spree::User.new(user_params)
+    # @user.spree_roles << Spree::Role.find_or_create_by(name: 'vendor') if !@user.has_spree_role?('vendor')
+    # @user.phone_number = contact_details_params[:phone]
+    @stock_location = Spree::StockLocation.new(stock_location_params)
+    # if Spree::Vendor.exists?(name: vendor_params[:name])
+    #   @vendor = Spree::Vendor.find_by(name: vendor_params[:name])
+    # else
+      @vendor = Spree::Vendor.new(vendor_params)
+    # end
+
+    if all_params[:image].present?
+      params[:uploaded_file] = all_params[:image].read # File contents
+      params[:uploaded_file_original_filename] = all_params[:image].original_filename
+      params[:uploaded_file_headers] = all_params[:image].headers
+      params[:uploaded_file_content_type] = all_params[:image].content_type
+      @vendor.build_image(attachment: all_params[:image]) 
+    elsif params[:uploaded_file].present? # a file coming through the form-resubmit
+      # generate an ActionDispatch::Http::UploadedFile
+      # tempfile = Tempfile.new("#{params[:uploaded_file_original_filename]}-#{Time.now}")
+      filename = params[:uploaded_file_original_filename]
+      tempfile = Tempfile.new("#{filename}")
+      tempfile.binmode
+      tempfile.write CGI.unescape(params[:uploaded_file]) #content of the file / unescaped
+      tempfile.close
+      file = ActionDispatch::Http::UploadedFile.new(
+        :content_type =>  params[:uploaded_file_content_type],
+        :original_filename => params[:uploaded_file_original_filename],
+        :tempfile => tempfile,
+        :filename => params[:uploaded_file_original_filename],
+        :head => params[:uploaded_file_headers],
+        :type => params[:uploaded_file_content_type]
+      )
+      # merge into the params
+      @vendor.build_image.attachment.attach(io: StringIO.new(CGI.unescape(params[:uploaded_file])), filename: filename, content_type: params[:uploaded_file_content_type])  
+      # @vendor.build_image.attachment.attach(CGI.unescape(params[:uploaded_file])) # invalid email
+
+      all_params.merge!(image: file)
+      # @vendor.build_image(attachment: file) # works only once
+      # @vendor.build_image.attachment.attach(all_params[:image]) # invalid email
+      
+      # params.merge!(image: 
+      #    ActionDispatch::Http::UploadedFile.new(
+      #                             :tempfile => tempfile,
+      #                             :filename => params[:uploaded_file_original_filename],
+      #                             :head => params[:uploaded_file_headers],
+      #                             :type => params[:uploaded_file_content_type]
+      #                        )
+      #              )
+      # @vendor.build_image(attachment: params[:image]) 
+
+    end
+
+
+    @stock_location.name = 'name'
+    @vendor.name = "name#{Spree::Vendor.count + 1}"
+    @vendor.save!
+    @vendor.valid?
+    # @user.valid?
+    if @stock_location.valid? && @vendor.valid?
+      flash[:notice] = 'valid' 
+    else
+      message=''
+      if @vendor.errors.any?
+        message += 'Vendor Account Details: ' + @vendor.errors.full_messages.compact * ' and ' + '. '
+      end
+      # if @user.errors.any?
+      #   message += 'Account Details: ' + @user.errors.full_messages.compact * ' and ' + '. '
+      # end
+      if @stock_location.errors.any?
+        message += 'Stock Location: ' + @stock_location.errors.full_messages.compact * ' and ' + '. '
+      end
+      flash[:error] = message
+    end
+    render action: 'new'
   end
 
   def create
     @user = Spree::User.new(user_params)
     @user.spree_roles << Spree::Role.find_or_create_by(name: 'vendor') if !@user.has_spree_role?('vendor')
-    @user.phone_number = contact_details_params[:phone]
+    # @user.phone_number = contact_details_params[:phone]
     @stock_location = Spree::StockLocation.new(stock_location_params)
     @vendor = Spree::Vendor.new(vendor_params)
     if @user.valid? && @vendor.valid? && @stock_location.valid?
