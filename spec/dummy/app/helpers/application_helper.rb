8bb89a74d5b6285958871bc02686af0f4cb60bf3
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-12-19 23:56:47 +0300

    fix for prev commit

--- spec/dummy/app/helpers/application_helper.rb
+++ spec/dummy/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def show_errors(object, field_name)
-    if object.errors.any?
+    if object && object.errors.any?
       if !object.errors.messages[field_name].blank?
         "#{"#{field_name}".split('_').join(' ')} #{object.errors.messages[field_name].join(' and ')}".capitalize
       end
@@ -8,7 +8,7 @@ module ApplicationHelper
   end  
   
   def is_error(object, field_name)
-    object.errors.any? && !object.errors.messages[field_name].blank? ? true : false
+    object && object.errors.any? && !object.errors.messages[field_name].blank? ? true : false
   end
 
   def error_class(object, field_name)
