cbc322398af41dfc1195e28e7ee8461495391f36
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-12-19 23:16:30 +0300

    add validation error to uniq fields, add labels for account details

--- spec/dummy/app/helpers/application_helper.rb
+++ spec/dummy/app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def show_errors(object, field_name)
+    if object.errors.any?
+      if !object.errors.messages[field_name].blank?
+        "#{"#{field_name}".split('_').join(' ')} #{object.errors.messages[field_name].join(' and ')}".capitalize
+      end
+    end
+  end  
+  
+  def is_error(object, field_name)
+    object.errors.any? && !object.errors.messages[field_name].blank? ? true : false
+  end
+
+  def error_class(object, field_name)
+    is_error(object, field_name) ? 'is-invalid' : ''
+  end
 end
