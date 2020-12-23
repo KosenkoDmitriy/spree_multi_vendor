d4261853e1ef051b17223bead4097a57255ab3fa
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2020-12-23 18:30:27 +0300

    backup create method for vendor complex sign up form

--- app/controllers/spree/vendors_controller.rb
+++ app/controllers/spree/vendors_controller.rb
@@ -150,6 +150,77 @@ class Spree::VendorsController < Spree::StoreController
     end
   end
 
+  def create_full
+    @user = Spree::User.new(user_params)
+    @user.spree_roles << Spree::Role.find_or_create_by(name: 'vendor') if !@user.has_spree_role?('vendor')
+    # @user.phone_number = contact_details_params[:phone] # twilio ask for a new phone number during verification process
+    @stock_location = Spree::StockLocation.new(stock_location_params)
+    @vendor = Spree::Vendor.new(vendor_params)
+
+    # remember vendor's image after validation error
+    if all_params[:image].present?
+      params[:uploaded_file] = all_params[:image].read # File contents
+      params[:uploaded_file_original_filename] = all_params[:image].original_filename
+      params[:uploaded_file_headers] = all_params[:image].headers
+      params[:uploaded_file_content_type] = all_params[:image].content_type
+    elsif params[:uploaded_file].present? # a file coming through the form-resubmit
+      # generate an ActionDispatch::Http::UploadedFile
+      filename = params[:uploaded_file_original_filename]
+      tempfile = Tempfile.new("#{filename}-#{Time.now}")
+      tempfile.binmode
+      tempfile.write StringIO.new(CGI.unescape(params[:uploaded_file])) #content of the file / unescaped
+      tempfile.close
+      file = ActionDispatch::Http::UploadedFile.new(
+        content_type: params[:uploaded_file_content_type],
+        headers: params[:uploaded_file_headers],
+        original_filename: params[:uploaded_file_original_filename],
+        tempfile: tempfile,
+        filename: params[:uploaded_file_original_filename],
+        head: params[:uploaded_file_headers],
+        type: params[:uploaded_file_content_type]
+      )
+      # merge into the params
+      params[:spree_vendor] = params[:spree_vendor].merge!(image: file)
+    end
+
+    if @user.valid? && @vendor.valid? && @stock_location.valid?
+      @user.save  
+      @vendor.contact_us = contact_details_params.to_h.map{|key,value| key + ': ' + value }.join("\n")
+      @vendor.users << @user
+      begin
+        @vendor.build_image(attachment: all_params[:image])
+      rescue
+      end
+      if @vendor.save
+        # @vendor.stock_locations = [@stock_location]
+        last_stock_location = @vendor.stock_locations.last
+        last_stock_location.update_attributes(stock_location_params)
+        # last_stock_location.update(state_name: last_stock_location.name)
+        flash[:notice] = I18n.t(:'spree.vendor_registrations.inactive_signed_up')
+        redirect_to root_path
+        return
+      else
+        flash.now[:error] = 'error - can not create the vendor'
+      end
+    else
+      @vendor.valid?
+      @user.valid?
+      @stock_location.valid?
+      message=''
+      if @vendor.errors.any?
+        message += 'Vendor Account Details: ' + @vendor.errors.full_messages.compact * ' and ' + '. '
+      end
+      if @user.errors.any?
+        message += 'Account Details: ' + @user.errors.full_messages.compact * ' and ' + '. '
+      end
+      if @stock_location.errors.any?
+        message += 'Stock Location: ' + @stock_location.errors.full_messages.compact * ' and ' + '. '
+      end
+      flash.now[:error] = message
+      render action: 'new'
+    end
+  end
+
   private
   def all_params
     params.require(:spree_vendor).permit(:name, :about_us, :image, :notification_email,
