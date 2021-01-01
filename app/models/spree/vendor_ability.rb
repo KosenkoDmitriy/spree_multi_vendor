89319d85f81a36458a4f19b748384e153575d161
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2021-01-02 02:38:13 +0300

    vendor can not view the option types of another vendor

--- app/models/spree/vendor_ability.rb
+++ app/models/spree/vendor_ability.rb
@@ -48,8 +48,9 @@ class Spree::VendorAbility
   end
 
   def apply_option_type_permissions
-    # cannot_display_model(Spree::OptionType)
+    cannot_display_model(Spree::OptionType)
     # can :read, Spree::OptionType, name: 'condition'
+    can :read, Spree::OptionType, vendor_id: nil
     can :manage, Spree::OptionType, vendor_id: @vendor_ids
     can :create, Spree::OptionType
   end
@@ -59,7 +60,6 @@ class Spree::VendorAbility
   end
 
   def apply_product_option_type_permissions
-    # can :read, Spree::OptionType, name: 'condition'
     can :modify, Spree::ProductOptionType, product: { vendor_id: @vendor_ids }
   end
 
